93cfca558f9b1a0386e048b304ea183d5707ad57
Praveen Kumar <praveen170596@gmail.com>
2022-11-17 09:55:16 +0530

    Created a Spring container and loading the configuration from the EmployeeConfig class.

--- src/main/java/com/niit/jdp/App.java
+++ src/main/java/com/niit/jdp/App.java
@@ -1,10 +1,14 @@
 package com.niit.jdp;
 
+import com.niit.jdp.config.EmployeeConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Creating a Spring container and loading the configuration from the EmployeeConfig class.
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(EmployeeConfig.class)
     }
 }
