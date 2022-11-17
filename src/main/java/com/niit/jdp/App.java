7873e531ac7a10baedb2159d6d87c31eb0d7eafa
Praveen Kumar <praveen170596@gmail.com>
2022-11-17 09:05:49 +0530

    created Employee.java class.

--- src/main/java/com/niit/jdp/App.java
+++ src/main/java/com/niit/jdp/App.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.niit.jdp;
 
 /**
  * Hello world!
---
+++ src/main/java/com/niit/jdp/domain/Employee.java
@@ -0,0 +1,11 @@
+/*
+ *Author Name:Praveen Kumar
+ *Date: 17-Nov-22
+ *Created With IntelliJ Idea Community Edition
+ */
+
+
+package com.niit.jdp.domain;
+
+public class Employee {
+}
