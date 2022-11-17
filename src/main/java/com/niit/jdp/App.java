58863f3986d8a30e18f15a684909adfd9a7a4d9d
Praveen Kumar <praveen170596@gmail.com>
2022-11-17 09:57:21 +0530

    Getting the bean from the Spring container.

--- src/main/java/com/niit/jdp/App.java
+++ src/main/java/com/niit/jdp/App.java
@@ -1,6 +1,7 @@
 package com.niit.jdp;
 
 import com.niit.jdp.config.EmployeeConfig;
+import com.niit.jdp.domain.Employee;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -10,5 +11,9 @@ public class App {
     public static void main(String[] args) {
         // Creating a Spring container and loading the configuration from the EmployeeConfig class.
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(EmployeeConfig.class)
+
+        // Getting the bean from the Spring container.
+        Employee employee = context.getBean("employee1", Employee.class);
+
     }
 }
