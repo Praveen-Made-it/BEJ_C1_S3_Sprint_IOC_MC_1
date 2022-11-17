e444ad8b89553902cd5225f1688458acffd0a800
Praveen Kumar <praveen170596@gmail.com>
2022-11-17 09:34:18 +0530

    create the Empoyee method

--- src/main/java/com/niit/jdp/config/EmployeeConfig.java
+++ src/main/java/com/niit/jdp/config/EmployeeConfig.java
@@ -7,6 +7,15 @@
 
 package com.niit.jdp.config;
 
+import com.niit.jdp.domain.Employee;
+import org.springframework.context.annotation.Bean;
+
 public class EmployeeConfig {
+    @Bean("employee1")
+    // A method which returns an object of type Employee.
+    public Employee getEmployee() {
+
+
+    }
 
 }
